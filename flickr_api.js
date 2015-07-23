995ca18abf7704d6afa4c092e714da2572b0bda2
marvinick <marviluw@gmail.com>
2015-07-23 12:24:56 -0700

    make AJAX request -- flickr_api

--- flickr_api.js
+++ flickr_api.js
@@ -2,5 +2,14 @@ $(document).ready(function () {  //make sure the page is fully loaded
   $('button').click(function () {
     $("button").removeClass("selected"); //highlight off when the pointer move away
     $("button").addClass("selected"); // highlight on when the pointer hovers
+    var flickerAPI = "http://api.flicker.com/services/feeds/photo_public.gne?jsoncallback=?";
+    var animal = $(this).text();
+    var flickrOptions = {
+      tags: animal,
+      format: "json",
+    };
+    function displayPhotos(data) {
+    }
+    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
   });
 });
