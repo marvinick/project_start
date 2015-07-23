25e801353bc7c4cdaafc063b7ece19cd6032b274
marvinick <marviluw@gmail.com>
2015-07-23 13:07:35 -0700

    displaying the photos -- flickr_api

--- flickr_api.js
+++ flickr_api.js
@@ -9,6 +9,14 @@ $(document).ready(function () {  //make sure the page is fully loaded
       format: "json",
     };
     function displayPhotos(data) {
+      var photoHTML = '<ul>';
+      $.each (data.items, function (i, photo) {
+        photoHTML += '<li class="grid-25 tablet-grid-50">';
+        photoHTML += '<a href="' + photo.link + '" class="image">';
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+      });
+      photoHTML += '</ul>';
+      $('#photos').html(photoHTML);
     }
     $.getJSON(flickerAPI, flickrOptions, displayPhotos);
   });
