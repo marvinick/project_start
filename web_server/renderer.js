ead681828d1ba8362df5f68911e8ad6c75dfbc4c
marvinick <marviluw@gmail.com>
2015-07-26 08:01:30 -0700

    merger values

--- web_server/renderer.js
+++ web_server/renderer.js
@@ -1,11 +1,21 @@
 var fs = require ('fs');
 
+function mergeValues(values, content) {
+  //cycle over the keys
+  for (var key in values) {
+    //replace all {{key}} with the values from values object
+    content = content.replace("{{" + key + "}}", values[key]);
+  }
+    //return merged content
+    return content;
+}
+
 function view (templateName, values, response) {
   //read from the template file
-  var fileContents = fs.readFileSync('./views/' + templateName + '.html');
+  var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
 
   //insert values in to the content
-
+  fileContents = mergeValues(values, fileContents);
   //write out to the response
   response.write(fileContents);
 }
