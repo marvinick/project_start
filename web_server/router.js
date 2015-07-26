19649efd1d70b87e761ffd4b6363a4fc9d732652
marvinick <marviluw@gmail.com>
2015-07-25 19:48:13 -0700

    web_server -- making our vies DRY

--- web_server/router.js
+++ web_server/router.js
@@ -1,3 +1,5 @@
+var Profile = require("./profile.js");
+
 //Handle HTTP route GET / and POST / i.e Home
 function home(request, response) {
   //if url == "/" && GET
@@ -20,8 +22,31 @@ function user(request, response) {
   if(username.length > 0 ) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write("Header\n");
-    response.write(username + "\n");
-    response.end('Footer\n');
+
+    //get json from Treehouse
+    var studentProfile = new Profile(username);
+    //on 'end'
+    studentProfile.on('end', function(profileJSON) {
+      //show profile
+
+      //store values we need
+      var values = {
+        avatarUrl: profileJSON.gravatar_url,
+        username: profileJSON.user_name,
+        badges: profileJSON.badges.length,
+        javascriptPoints: profileJSON.points.JavaScript
+      }
+      //simple response
+      response.write(values.username + " has " + values.badges +  " badges\n");
+      response.end('Footer\n');
+    });
+
+    //on 'error'
+    studentProfile.on('error', function(error) {
+      //show error
+      response.write(error.message + "\n")
+      response.end('Footer\n');
+    });
   }
 }
 
